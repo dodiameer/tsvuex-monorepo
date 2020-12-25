76efd96c968acd42851d544409fee7d49189955f
= <=>
2020-12-25 14:16:06 +0300

    initial template

---
+++ packages/client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    Hello, World!
+  </div>  
+</template>
+<script lang="ts">
+import Component from "vue-class-component"
+import Vue from "vue"
+
+@Component
+export default class App extends Vue {}
+</script>
+<style>
+
+</style>
